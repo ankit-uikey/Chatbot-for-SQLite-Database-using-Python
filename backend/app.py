bbb47af861441ae06ec79f75920f10bda412b071
ankit-uikey <ankit00741@gmail.com>
2025-02-06 16:29:42 +0530

    Added waitress serve

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 from chatbot import process_query
 from flask_cors import CORS
 #from threading import Thread
-#from waitress import serve
+from waitress import serve
 import os
 
 app = Flask(__name__, static_folder="../frontend")
@@ -26,8 +26,8 @@ def chat():
     response = process_query(user_input)
     return jsonify({"response": response})
 
-#def run_flask():
-#    app.run(host="0.0.0.0", port=0)
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=8501)
+    
+#if __name__ == "__main__":
+#    app.run(debug=True)
